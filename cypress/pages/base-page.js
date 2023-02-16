0e5c7818d88e07306278fb73fe666688a28a5b8d
AmminaSK <75207330+AmminaSK@users.noreply.github.com>
2023-02-16 17:08:17 +0100

    moved method from index.js

--- cypress/pages/base-page.js
+++ cypress/pages/base-page.js
@@ -1,3 +1,5 @@
+import imaps from "imap-simple";
+
 const S = require('../fixtures/settings')
 const D = require('../fixtures/data')
 // *************************** ELEMENTS ***************************
@@ -381,5 +383,61 @@ export default class BasePage {
         let resultF = result.trim();
         return resultF;
     }
+
+    fetchGmailUnseenMails({username, password, markSeen}) {
+        var config = {
+            imap: {
+                user: username,
+                password: password,
+                host: 'imap.gmail.com',
+                port: 993,
+                tls: true,
+                authTimeout: 5000
+            }
+        };
+
+        return imaps.connect(config).then(function (connection) {
+
+            return connection.openBox('INBOX').then(function () {
+                var searchCriteria = [
+                    'UNSEEN'
+                ];
+
+                var fetchOptions = {
+                    bodies: ['HEADER', 'TEXT'],
+                    markSeen: markSeen
+                };
+
+                return connection.search(searchCriteria, fetchOptions).then(function (results) {
+                    var mails = [];
+                    if (results) {
+                        results.forEach(item => {
+                            var _mail = {};
+                            if (item && item.parts) {
+                                item.parts.forEach(_item => {
+                                    if (_item.which == "TEXT") {
+                                        _mail.body = _item.body;
+                                    }
+                                    if (_item.which == "HEADER" && _item.body && _item.body.from && _item.body.from.length) {
+                                        _mail.from = _item.body.from[0];
+                                    }
+                                    if (_item.which == "HEADER" && _item.body && _item.body.subject && _item.body.subject.length) {
+                                        _mail.subject = _item.body.subject[0];
+                                    }
+                                    if (_item.which == "HEADER" && _item.body && _item.body.to && _item.body.to.length) {
+                                        _mail.to = _item.body.to[0];
+                                    }
+                                })
+                            }
+                            mails.push(_mail);
+                        });
+                        //connection.end()
+                    }
+                    return mails;
+                });
+            });
+        });
+    },
+
 
 }
